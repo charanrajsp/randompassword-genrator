760523802b1139824af0fe255007ad685d9ff4e9
CHARANRAJ PATTAR <charanrajpattar96@gmail.com>
2025-01-30 23:38:43 +0530

    The Password Generator Web Application is a user-friendly tool designed to create secure and customizable passwords.
    This project is built using HTML, CSS, and JavaScript, focusing on security and ease of use. The application dynamically generates passwords based on user-selected preferences and provides a simple interface for quick password creation. It also includes a copy feature to easily copy the generated password.
    
    Technology Stack:
    HTML for structure
    CSS for styling and animations
    JavaScript for logic and functionality

---
+++ passwordgen.js
@@ -0,0 +1,60 @@
+// Select Elements
+const passwordField = document.getElementById("Password");
+const lengthSlider = document.getElementById("pgrbar");
+const rangeValue = document.getElementById("rangeValue");
+const includeNumbers = document.getElementById("number");
+const includeSpecial = document.getElementById("splchar");
+const includeAlphabets = document.getElementById("alpchar");
+
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '-', '{', '}', '[', ']', ';', ':', '<', '>', ',', '.', '?', '/'];
+const alphabets = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz']; // Spreads letters into an array
+
+// Update range value display
+function updateRangeValue() {
+    rangeValue.textContent = `Length: ${lengthSlider.value}`;
+}
+
+// Function to Generate Password
+function generatePassword() {
+    const length = parseInt(lengthSlider.value);
+    let characters = [];
+    let password = "";
+
+    if (includeNumbers.checked) 
+        {
+        characters = characters.concat(numbers);
+    }
+    if (includeSpecial.checked) 
+        {
+        characters = characters.concat(specialChars);
+    }
+    if (includeAlphabets.checked)
+         {
+        characters = characters.concat(alphabets);
+    }
+
+    if (characters.length === 0) 
+        {
+        alert("Please select at least one option!");
+        return;
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    passwordField.value = password;
+}
+
+// Copy Password to Clipboard
+function copyPassword() {
+    if (passwordField.value) {
+        navigator.clipboard.writeText(passwordField.value);
+        alert("Password copied to clipboard!");
+    } else {
+        alert("Generate a password first!");
+    }
+}
