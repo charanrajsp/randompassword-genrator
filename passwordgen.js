b61754b8d4b6a8917fe2403fab57e9caad9db419
CHARANRAJ PATTAR <charanrajpattar96@gmail.com>
2025-02-01 17:00:38 +0530

    Update 0.2

--- passwordgen.js
+++ passwordgen.js
@@ -8,8 +8,9 @@ const includeAlphabets = document.getElementById("alpchar");
 
 
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '-', '{', '}', '[', ']', ';', ':', '<', '>', ',', '.', '?', '/'];
-const alphabets = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz']; // Spreads letters into an array
+const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', 
+    '_', '+', '=', '-', '{', '}', '[', ']', ';', ':', '<', '>', ',', '.', '?', '/'];
+const alphabets = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz']; // Merge & Spreads letters into an array
 
 //  range value display
 function updateRangeValue() 
@@ -21,8 +22,11 @@ function updateRangeValue()
 function generatePassword() 
 {
     const length = parseInt(lengthSlider.value);
+
     let characters = [];
+
     let password = "";
+
 
     if (includeNumbers.checked) 
         {
